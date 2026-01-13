ed0df12f50c748f9660e488fb06ecf22a3eb40bf
Fitman22 <85381077+Fitman22@users.noreply.github.com>
2026-01-12 19:30:22 -0500

    add_basic_functions

---
+++ Unity_project/Mycrobiology_project/Assets/Controls/ControlsManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class ControlsManager : MonoBehaviour
+{
+    Map map;
+    static Map.ControlsActions controls;
+    public static ControlsManager instance;
+    private void Awake()
+    {        
+        map = new Map();
+        controls= map.controls;
+        instance=this;
+    }
+    public static Map.ControlsActions getControls() { return controls; }
+    private void OnEnable()
+    {
+        map.Enable();
+    }
+    private void OnDisable()
+    {
+        map.Disable();
+    }
+  
+}
