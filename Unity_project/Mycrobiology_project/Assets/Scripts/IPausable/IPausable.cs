cd00bbbce73cc20d24457608d32b043f887f0aeb
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2026-01-16 17:48:52 -0500

    crear interfaz pausable para cuando se pausa

---
+++ Unity_project/Mycrobiology_project/Assets/Scripts/IPausable/IPausable.cs
@@ -0,0 +1,10 @@
+// Interfaz para los objetos pausables
+
+public interface IPausable
+{
+
+    // Lógica que se encarga de pausar el script
+    // y las preparaciones previas
+    public void Pause();
+
+}
