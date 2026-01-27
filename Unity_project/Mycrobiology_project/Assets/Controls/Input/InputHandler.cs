8a2fa3f7585d4ce8badcbbbc3e31b6d2d22628b9
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2026-01-27 16:54:55 -0500

    comentar las salidas por consola

--- Unity_project/Mycrobiology_project/Assets/Controls/Input/InputHandler.cs
+++ Unity_project/Mycrobiology_project/Assets/Controls/Input/InputHandler.cs
@@ -3,54 +3,29 @@ using UnityEngine.InputSystem;
 
 public class InputHandler : MonoBehaviour
 {
-    // private Camera mainCamera;
-
-    // public void Start()
-    // {
-    //     mainCamera = Camera.main;
-    //     ControlsManager.getControls().Interact.started += OnClick;
-    // }
-
-    // public void OnClick(InputAction.CallbackContext context)
-    // {
-    //     // Debug.Log("Entre a click");
-
-    //     if (!context.started) return;
-    //     // Debug.Log("El contexto es started");
-
-    //     RaycastHit2D rayHit = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue()));
-
-    //     if (!rayHit.collider) return;
-
-    //     Debug.Log($"La posicion del mouse es: {Mouse.current.position.ReadValue()}");
-    //     Debug.Log($"Se hizo click sobre el objeto {rayHit.collider.gameObject.name}");
-    // }
-
-    // ===================== WOKRING VERSION =====================
-
     [SerializeField]
     private Raycaster raycaster;
 
     public void Start()
     {
         ControlsManager.getControls().Interact.started += OnTap;
-        Debug.Log("Se ha suscrito interact al evento on tap");
+        // Debug.Log("Se ha suscrito interact al evento on tap");
     }
 
     public void OnDestroy()
     {
         ControlsManager.getControls().Interact.started -= OnTap;
-        Debug.Log("Desuscrito interact al evento on tap");
+        // Debug.Log("Desuscrito interact al evento on tap");
     }
 
     public void OnTap(InputAction.CallbackContext context)
     {
-        Debug.Log("Se ha entrado a OnTap");
+        // Debug.Log("Se ha entrado a OnTap");
         if (!context.started) return;
 
         Vector2 tapPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 
-        Debug.Log($"La posicion del tap es: {tapPosition}");
+        // Debug.Log($"La posicion del tap es: {tapPosition}");
 
         raycaster.ProcessTap(tapPosition);
     }
--- Unity_project/Mycrobiology_project/Assets/Scripts/Raycaster/Raycaster.cs
+++ Unity_project/Mycrobiology_project/Assets/Scripts/Raycaster/Raycaster.cs
@@ -12,20 +12,20 @@ public class Raycaster : MonoBehaviour
 
     public void ProcessTap(Vector2 screenPosition)
     {
-        Debug.Log("Procesando tap en Raycaster");
+        // Debug.Log("Procesando tap en Raycaster");
         Ray ray = mainCamera.ScreenPointToRay(screenPosition);
 
         RaycastHit2D rayHit = Physics2D.GetRayIntersection(ray);
 
         if (!rayHit.collider) return;
 
-        Debug.Log($"Se hizo tap sobre el objeto {rayHit.collider.gameObject.name}");
+        // Debug.Log($"Se hizo tap sobre el objeto {rayHit.collider.gameObject.name}");
 
         rayHit.collider.TryGetComponent<ITappable>(out ITappable tappable);
 
         if (tappable == null) return;
 
-        Debug.Log("El objeto es ITappable, llamando OnTap()");
+        // Debug.Log("El objeto es ITappable, llamando OnTap()");
         tappable.OnTap();
 
 
