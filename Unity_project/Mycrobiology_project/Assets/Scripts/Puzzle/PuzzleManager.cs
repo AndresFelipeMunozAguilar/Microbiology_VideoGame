ab9926492458af36c79162f04cb8873422085c59
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2026-01-28 17:33:40 -0500

    Instanciar fondo con zona activa al tocar el Puzzle

--- Unity_project/Mycrobiology_project/Assets/Scripts/Puzzle/PuzzleManager.cs
+++ Unity_project/Mycrobiology_project/Assets/Scripts/Puzzle/PuzzleManager.cs
@@ -6,13 +6,24 @@ public class PuzzleManager : MonoBehaviour, ITappable
     [SerializeField]
     private PuzzleHalo halo;
 
+    [SerializeField]
+    private GameObject background;
+
     public void OnTap()
     {
         if (halo.isPlayerClose)
         {
-            Debug.Log($"Tap detectado sobre: {this.gameObject.name}");
+            Vector3 inFrontOfCamera = Camera.main.transform.position;
+            inFrontOfCamera.z = 0f;
+            SpawnBackground(inFrontOfCamera, Quaternion.identity, this.transform);
+
         }
 
+    }
+
+    public void SpawnBackground(Vector3 position, Quaternion rotation, Transform parent)
+    {
+        Instantiate(background, position, rotation, parent);
     }
 
 }
