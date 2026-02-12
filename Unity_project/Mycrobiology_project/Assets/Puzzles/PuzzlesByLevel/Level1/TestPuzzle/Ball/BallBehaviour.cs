129b017d47e26895aa5e27a595f8a8edd0eb5be2
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2026-02-11 23:28:03 -0500

    Crear comportamiento para arrastrar la bola basado en la posición del tap

--- Unity_project/Mycrobiology_project/Assets/Puzzles/PuzzlesByLevel/Level1/TestPuzzle/Ball/BallBehaviour.cs
+++ Unity_project/Mycrobiology_project/Assets/Puzzles/PuzzlesByLevel/Level1/TestPuzzle/Ball/BallBehaviour.cs
@@ -1,8 +1,50 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class BallBehaviour : MonoBehaviour, IPointerDownHandler
+public class BallBehaviour : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
+    [SerializeField]
+    private Canvas canvas;
+
+    [SerializeField]
+    private Camera mainCamera;
+
+    [SerializeField]
+    private float zDistanceToCamera;
+
+    public void Start()
+    {
+        mainCamera = Camera.main;
+
+        // Se guarda la distancia Z entre el objeto y la cámara para que no salte al arrastrar
+        zDistanceToCamera = Mathf.Abs(mainCamera.transform.position.z - transform.position.z);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // Se obtiene la posición ABSOLUTA del puntero en pantalla (píxeles)
+        Vector3 screenPointerPosition = eventData.position;
+
+        // Se le asigna la profundidad Z que el objeto ya tenía en el mundo
+        screenPointerPosition.z = zDistanceToCamera;
+
+        // Se convierte esa posición de pantalla a una coordenada exacta en el mundo
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(screenPointerPosition);
+
+        // Se aplica la posición al objeto (manteniendo su Z original si es necesario)
+        transform.position = new Vector3(worldPosition.x, worldPosition.y, transform.position.z);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+    }
+
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("Basket BALL: OnPOinterDown");
