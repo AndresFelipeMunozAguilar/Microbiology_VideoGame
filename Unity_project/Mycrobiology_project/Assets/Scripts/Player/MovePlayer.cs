d0af04912a6bd2f1aef2b40b035f610342c30318
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2026-02-21 21:26:28 -0500

    Restaurar el movimiento del Player mediante joystick

--- Unity_project/Mycrobiology_project/Assets/Scripts/Player/MovePlayer.cs
+++ Unity_project/Mycrobiology_project/Assets/Scripts/Player/MovePlayer.cs
@@ -10,20 +10,8 @@ public class MovePlayer : MonoBehaviour, IPausable, IPuzzlePausable
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        ControlsManager.getControls().PickUp.performed += ctr => pickup();
-        ControlsManager.getControls().Pickdown.performed += ctr => pickdown();
 
         GameManager.GetInstance().SubscribePuzzlePausable(this);
-    }
-
-    void pickup()
-    {
-        if (objectCollision) objectCollision.SetParent(this.transform);
-    }
-
-    void pickdown()
-    {
-        transform.GetChild(0).SetParent(null);
     }
 
     void OnCollisionEnter2D(Collision2D other)
@@ -39,12 +27,12 @@ public class MovePlayer : MonoBehaviour, IPausable, IPuzzlePausable
         }
     }
 
-    void Update()
+    void FixedUpdate()
     {
         input = ControlsManager.getControls().Move.ReadValue<Vector2>();
         if (input.sqrMagnitude > 0.01f)
         {
-            Vector2 targetPos = rb.position + input * speed * Time.deltaTime;
+            Vector2 targetPos = rb.position + input * speed * Time.fixedDeltaTime;
             rb.MovePosition(targetPos);
         }
     }
