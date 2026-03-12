a87e87f3a054f913538b38fda54bb1e7ae9fa4c2
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2026-03-12 01:29:21 -0500

    Cambiar la manera en la que se muestra el nombre del objeto colisionado: TextMesPro -- Reemplazado por --> Debug.Log

--- Unity_project/Mycrobiology_project/Assets/Scripts/Raycaster/Raycaster.cs
+++ Unity_project/Mycrobiology_project/Assets/Scripts/Raycaster/Raycaster.cs
@@ -16,7 +16,9 @@ public class Raycaster : MonoBehaviour
         Ray ray = mainCamera.ScreenPointToRay(screenPosition);
 
         RaycastHit2D rayHit = Physics2D.GetRayIntersection(ray);
-        ControlsManager.instance.setError(rayHit.collider.name.ToString());
+
+        Debug.Log($"<color=orange>Raycaster:</color> RaycastHit2D detectó colision con {(rayHit.collider != null ? rayHit.collider.gameObject.name : "ningún objeto")}");
+
         if (!rayHit.collider) return;
 
         Debug.Log($"Se hizo tap sobre el objeto {rayHit.collider.gameObject.name}");
@@ -27,7 +29,5 @@ public class Raycaster : MonoBehaviour
 
         Debug.Log("El objeto es ITappable, llamando OnTap()");
         tappable.OnTap();
-
-
     }
 }
