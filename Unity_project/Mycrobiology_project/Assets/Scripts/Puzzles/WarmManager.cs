2653bde5ad8469920665ee39d0eb0fb44ed13c58
Fitman22 <branyt29@gmail.com>
2026-03-18 00:00:42 -0500

    add_warm

--- Unity_project/Mycrobiology_project/Assets/Scripts/Puzzles/WarmManager.cs
+++ Unity_project/Mycrobiology_project/Assets/Scripts/Puzzles/WarmManager.cs
@@ -1,37 +1,13 @@
 using UnityEngine;
-using UnityEngine.UI;
+
 
 public class WarmManager : MonoBehaviour
 {
     [SerializeField] Transform warmPoint;
-    [SerializeField] Image WarmBar;
-    [SerializeField] float speedWarm;
 
-    bool IsWarming;
-
-    void Update()
+    public void StartWarming(ElementManager element)
     {
-        if (IsWarming)
-        {
-            WarmBar.fillAmount+=0.01f*speedWarm*Time.deltaTime;
-        }
-    }
-
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.TryGetComponent<ElementManager>(out ElementManager element))
-        {
-            IsWarming=true;
-            element.PlaceWarm(warmPoint.position);
-        }
-    }
-    void OnTriggerExit2D(Collider2D collision)
-    {
-        if(collision.TryGetComponent<ElementManager>(out ElementManager element))
-        {
-            IsWarming=false;
-            WarmBar.fillAmount=0;
-        }
+        element.PlaceWarm(warmPoint.position);
     }
 
 
