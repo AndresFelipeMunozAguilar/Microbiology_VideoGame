05a03b7ee05ee087a0a383a6d7b3d9dd05fcda69
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2026-03-25 02:15:28 -0500

    Cambiar la propiedad de localPositioin a globalPosition para que  corresponda con el uso que se le dará

--- Unity_project/Mycrobiology_project/Assets/Puzzles/Commons/PuzzleGameplay/SpawnableElement/SpawnableElement.cs
+++ Unity_project/Mycrobiology_project/Assets/Puzzles/Commons/PuzzleGameplay/SpawnableElement/SpawnableElement.cs
@@ -5,6 +5,6 @@ public class SpawnableElement
 {
     public string name; // Solo para organización en el Inspector
     public GameObject prefab;
-    public Vector3 localPosition;
+    public Vector3 globalPosition;
     public Quaternion localRotation = Quaternion.identity;
 }
