81368faf15bdde2c75d390b378ab335e05cb3bcc
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2026-05-13 17:19:32 -0500

    Corregir typo

--- Unity_project/Mycrobiology_project/Assets/Puzzles/PuzzlesByLevel/Level1/BloodStainPuzzle/Bloodstain/ProcessFeedbackVisuals.cs
+++ Unity_project/Mycrobiology_project/Assets/Puzzles/PuzzlesByLevel/Level1/BloodStainPuzzle/Bloodstain/ProcessFeedbackVisuals.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
-public class ProccessFeedbackVisuals : MonoBehaviour
+public class ProcessFeedbackVisuals : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer _spriteRenderer;
 
